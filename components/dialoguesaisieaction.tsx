9b42546a828abef841dbcddf724b33beaa837149
esHome33 <etienschwa@gmail.com>
2023-09-22 15:36:22 +0200

    correction d'erreurs ...

--- components/dialoguesaisieaction.tsx
+++ components/dialoguesaisieaction.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { DialogTitle, Dialog, DialogContent, DialogContentText, DialogActions, Button, TextField, Typography } from '@mui/material';
 
 type Props = {
-    onOK: (action_name: string|null) => void;
+    onOK: (action_name: string | null) => void;
     open: boolean;
 }
 
@@ -18,7 +18,7 @@ const DialogueSaisieAction = (props: Props) => {
         props.onOK(name);
     }
 
-    
+
     const handleCancel: React.MouseEventHandler<HTMLButtonElement> = (e) => {
         e.preventDefault();
         props.onOK(null);
@@ -29,46 +29,45 @@ const DialogueSaisieAction = (props: Props) => {
         setName(e.target.value);
     }
 
-    const Dia = <Dialog
-        open={props.open}
-        onClose={props.onOK}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-    >
-        <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-            <DialogContentText
-                id="scroll-dialog-description"
-
-                tabIndex={-1}
-            >
-                <Typography variant='body2'>Indiquer le nom de l'action à créer</Typography>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Nom action"
-                    type="text"
-                    fullWidth
-                    value={name}
-                    variant="standard"
-                    onChange={changeName}
-                />
-            </DialogContentText>
-            <DialogActions>
-                <Button onClick={handleCancel}
-                    variant='contained'
-                    color='primary'
-                    className='bg-blue-500 text-white font-bold'
-                >Annuler</Button>
-                <Button onClick={handleClose}
-                    variant='contained'
-                    color='primary'
-                    className='bg-blue-700 text-white font-bold'
-                >Créer</Button>
-            </DialogActions>
-        </DialogContent>
-    </Dialog>
+    const Dia =
+        <Dialog
+            open={props.open}
+            aria-labelledby="scroll-dialog-title"
+            aria-describedby="scroll-dialog-description"
+        >
+            <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+                <DialogContentText
+                    id="scroll-dialog-description"
+                    tabIndex={-1}
+                >
+                    <Typography variant='body2'>Indiquer le nom de l&apos;action à créer</Typography>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Nom action"
+                        type="text"
+                        fullWidth
+                        value={name}
+                        variant="standard"
+                        onChange={changeName}
+                    />
+                </DialogContentText>
+                <DialogActions>
+                    <Button onClick={handleCancel}
+                        variant='contained'
+                        color='primary'
+                        className='bg-blue-500 text-white font-bold'
+                    >Annuler</Button>
+                    <Button onClick={handleClose}
+                        variant='contained'
+                        color='primary'
+                        className='bg-blue-700 text-white font-bold'
+                    >Créer</Button>
+                </DialogActions>
+            </DialogContent>
+        </Dialog>
 
 
     return (
